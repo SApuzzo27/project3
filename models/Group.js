059edfd507bc5b5b10f14042c1e1b017216d4c63
Sam <SApuzzo527@gmail.com>
2021-01-05 20:46:33 -0500

    added models

---
+++ models/Group.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GroupSchema = new Schema({
+clubName: {
+    type: String, 
+    required: true
+},
+
+users: 
+    {     
+        type: Array,
+    },
+    
+groupWatchList: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: "GroupWatchList"
+    }
+]
+
+});
+
+const Group = mongoose.model("Group", GroupSchema);
+
+module.exports = Group;
