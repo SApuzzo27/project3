320beb6ad11c1eb0b5c1304638a3af39202e1d8f
Sam <SApuzzo527@gmail.com>
2021-01-06 16:02:27 -0500

    added userwatchlist seed

--- models/UserWatchList.js
+++ models/UserWatchList.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserWatchListSchema = new Schema({
 	username: { type: String, required: true },
-    list: { type: Array},
+    // list: { type: Array},
     title: { type: String, required: true},
 	date: { type: Date, default: Date.now },
 });
--- utils/seedDB.js
+++ utils/seedDB.js
@@ -23,12 +23,36 @@ const commentsSeeds = [
    {
       body: "👾 another",
       username: "Admin"
+   }
+];
+const userWatchListSeeds = [
+   {
+      title:  " Step Brothers",
+      username: "Admin"
+   },
+   {
+      title:  "The Hang Over",
+      username: "Admin"
+   },
+   {
+      title:  " Wedding Crashers",
+      username: "Admin"
    },
 
 ];
+// const groupWatchListSeeds = [
+//    {
+//       clubName: "Group1",
+//       title: "Step Brothers",
+//    }
+// ]
+
+
 
 // remove all comments
 db.Comment.deleteMany({})
+// remove all userWatchList
+.then(() => db.UserWatchList.deleteMany({}))
 // remove all users
   .then(() => db.User.deleteMany({}))
   // add user
@@ -42,6 +66,14 @@ db.Comment.deleteMany({})
       // add comment ref to user
       .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { comments: _id } }, { new: true }))
   )
+  .then((user) => db.UserWatchList.create(userWatchListSeeds[0])
+      // add watchlist ref to user
+      .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { UserWatchList: _id } }, { new: true }))
+  )
+  .then((user) => db.UserWatchList.create(userWatchListSeeds[1])
+      // add watchlist ref to user
+      .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { UserWatchList: _id } }, { new: true }))
+  )
   .then(() => {
     process.exit(0);
   })
@@ -49,3 +81,4 @@ db.Comment.deleteMany({})
     console.error(err);
     process.exit(1);
   });
+
