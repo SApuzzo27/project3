35043c50c441d8523e5a3384bed34fcc4705c62a
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-07 01:24:48 -0500

    fixed case

--- utils/seedDB.js
+++ utils/seedDB.js
@@ -1,96 +1,115 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-const { mongoOptions } = require("./config")
+const { mongoOptions } = require("./config");
 
 // This file empties the Books collection and inserts the books below
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern",
-   mongoOptions
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  mongoOptions
 );
 
-const userSeed = 
-   {
-      username: "Admin",
-      email: "admin@contact.us",
-      password: "1"
-   }
-;
+const userSeed = {
+  username: "Admin",
+  email: "admin@contact.us",
+  password: "1",
+};
 const commentsSeeds = [
-   {
-      body: "🚀 initial seed",
-      username: "Admin"
-   },
-   {
-      body: "👾 another",
-      username: "Admin"
-   }
+  {
+    body: "🚀 initial seed",
+    username: "Admin",
+  },
+  {
+    body: "👾 another",
+    username: "Admin",
+  },
 ];
 const userWatchListSeeds = [
-   {
-      title:  " Step Brothers",
-      username: "Admin"
-   },
-   {
-      title:  "The Hang Over",
-      username: "Admin"
-   },
-   {
-      title:  " Wedding Crashers",
-      username: "Admin"
-   },
-
+  {
+    title: " Step Brothers",
+    username: "Admin",
+  },
+  {
+    title: "The Hang Over",
+    username: "Admin",
+  },
+  {
+    title: " Wedding Crashers",
+    username: "Admin",
+  },
 ];
 const groupSeeds = [
-   {
-      clubName: "Friday Night",
-    
-   }, 
-   {
-      clubName: "Sunday Afternoon"
-   }, 
-   {
-      clubName: "Saturday Night"
-   }
-]
-
-
+  {
+    clubName: "Friday Night",
+  },
+  {
+    clubName: "Sunday Afternoon",
+  },
+  {
+    clubName: "Saturday Night",
+  },
+];
 
 // remove all comments
 db.Comment.deleteMany({})
-// remove all userWatchList
-.then(() => db.UserWatchList.deleteMany({}))
-// remove all users
+  // remove all userWatchList
+  .then(() => db.UserWatchList.deleteMany({}))
+  // remove all users
   .then(() => db.User.deleteMany({}))
-      // add Group
-      .then(() => db.Group.create(groupSeeds))
+  // add Group
+  .then(() => db.Group.create(groupSeeds))
   // add user
   .then(() => db.User.create(userSeed))
   // add comments seeds
-  .then((user) => db.Comment.create(commentsSeeds[0])
+  .then((user) =>
+    db.Comment.create(commentsSeeds[0])
       // add comment ref to user
-      .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { comments: _id } }, { new: true }))
+      .then(({ _id }) =>
+        db.User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { comments: _id } },
+          { new: true }
+        )
+      )
   )
-  .then((user) => db.Comment.create(commentsSeeds[1])
+  .then((user) =>
+    db.Comment.create(commentsSeeds[1])
       // add comment ref to user
-      .then(({_id}) => {
-         console.log("id", _id)
-         db.User.findOneAndUpdate({_id: user._id}, { $push: { comments: _id } }, { new: true })
-      })
+      .then(({ _id }) =>
+        db.User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { comments: _id } },
+          { new: true }
+        )
+      )
   )
-  .then((user) => console.log("user", user))
-//   .then((user) => db.UserWatchList.create(userWatchListSeeds[0])
-//       // add watchlist ref to user
-//       .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { UserWatchList: _id } }, { new: true }))
-//   )
-//   .then((user) => db.UserWatchList.create(userWatchListSeeds[1])
-//       // add watchlist ref to user
-//       .then(({_id}) => db.User.findOneAndUpdate({_id: user._id}, { $push: { UserWatchList: _id } }, { new: true }))
-//   )
+  //.then((user) => console.log("user", user))
+  .then((user) =>
+    db.UserWatchList.create(userWatchListSeeds[0])
+      // add watchlist ref to user
+      .then(({ _id }) =>
+        db.User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { userWatchList: _id } },
+          { new: true }
+        )
+      )
+  )
+  .then((user) =>
+    db.UserWatchList.create(userWatchListSeeds[1])
+      // add watchlist ref to user
+      .then(({ _id }) =>
+        db.User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { userWatchList: _id } },
+          { new: true }
+        )
+      )
+  )
   .then(() => {
     process.exit(0);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     process.exit(1);
   });
-
