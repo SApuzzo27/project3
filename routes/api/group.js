721049cf497fcd28983615958c2d0444f5fb17a1
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-10 03:05:44 -0500

    added new routes

--- routes/api/group.js
+++ routes/api/group.js
@@ -1,4 +1,7 @@
-const router = require("exprexx").Router();
-const groupController = require("../../controllers/groupController");
+const router = require("express").Router();
+const groupController = require("../../controllers/groupsController");
+
+// Matches with "/api/group"
+router.route("/").get(groupController.findAll);
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,19 @@
 const router = require("express").Router();
 const commentRoutes = require("./comments");
 const userRoutes = require("./user");
+const groupRoutes = require("./group");
+const watchListRoutes = require("./watchlist");
+const movieRoutes = require("./movie");
 
 // comments routes
 router.use("/comments", commentRoutes);
 // user routes
 router.use("/user", userRoutes);
+// group routes
+router.use("/group", groupRoutes);
+//watchlist routes
+router.use("/watchlist", watchListRoutes);
+//movie routes
+router.use("/movie", movieRoutes);
 
 module.exports = router;
