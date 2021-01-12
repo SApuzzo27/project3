ec789ef7c2c03fa26901caa0782057cbe02a52b4
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-11 21:13:33 -0500

    moved login.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import './Login.css'
-import Home from './pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Groups from './pages/Groups';
-import Other from './pages/Other';
-import SignUp from './components/SignUpComponents';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import "./components/Login.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Groups from "./pages/Groups";
+import Other from "./pages/Other";
+import SignUp from "./components/SignUpComponents";
 
 function App() {
   return (
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/Groups' component={Groups} />
-          <Route path='/Other' component={Other} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
+          <Route path="/Groups" component={Groups} />
+          <Route path="/Other" component={Other} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
