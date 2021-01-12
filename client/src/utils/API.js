c5201942e60062fed7daaca97a22f39e9fd4909f
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-12 04:21:44 -0500

    updated API methods

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 const BASEURL = "https://www.omdbapi.com/?t=";
-const APIKEY = process.env.REACT_APP_API_KEY;
+const APIKEY = process.env.REACT_APP_API_KEY || "&apikey=trilogy";
 
 export default {
-  search: function(query) {
+  search: function (query) {
     return axios.get(BASEURL + query + APIKEY);
-  }
+  },
+  getAllSavedMovies: function () {
+    return axios.get("/api/movie");
+  },
+  saveMovie: function (movieData) {
+    return axios.post("/api/movie", movieData);
+  },
+  getUserByName: function (username) {
+    return axios.get("/api/clubuser/" + username);
+  },
 };
