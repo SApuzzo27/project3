8dcb2215ee3dc8890101f16a267081f6b4a4b646
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-13 03:11:01 -0500

    almost MVP

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import _ from "lodash";
+import React, { useState } from "react";
+//import _ from "lodash";
 
 //import OmdbContainer from "../components/OmdbContainer";
 import Container from "../components/Container";
@@ -7,68 +7,115 @@ import Card from "../components/Card";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import SearchForm from "../components/SearchForm";
-// import MovieDetail from "../components/MovieDetail";
+import Movie from "../components/Movie";
 import "../App.css";
 import API from "../utils/API";
 
 function User() {
-  const [currentUser, setCurrentUser] = useState({});
+  //const [currentUser, setCurrentUser] = useState({});
   const [searchResult, setSearchResult] = useState({});
-  const [userMovies, setUserMovies] = useState([]);
-  const [searchString, setSearchString] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  //const [userMovies, setUserMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  useEffect(() => {
-    if (_.isEmpty(currentUser)) {
-      loadUserByName("BobDobalina");
-    }
-    if (!_.isEmpty(currentUser)) {
-      loadUserMovies(currentUser);
-    }
-  }, [userMovies]);
+  // useEffect(() => {
+  //   if (_.isEmpty(currentUser)) {
+  //     loadUserByName("BobDobalina");
+  //   }
+  //   if (!_.isEmpty(currentUser)) {
+  //     loadUserMovies(currentUser);
+  //   }
+  // }, [userMovies, currentUser]);
 
-  function loadUserByName(userName) {
-    API.getUserByName(userName)
-      .then((res) => setCurrentUser(res.data))
-      .catch((err) => {
-        throw err;
-      });
-  }
+  // function loadUserByName(userName) {
+  //   console.log("loadUserByName");
+  //   API.getUserByName(userName)
+  //     .then((res) => setCurrentUser(res.data))
+  //     .catch((err) => {
+  //       console.log(err);
+  //       return 1;
+  //     });
+  // }
 
-  function handleInputChange(event) {
-    const { value } = event.target;
-    setSearchString(value);
-  }
-
-  function handleFormSubmit() {
-    API.search(searchString)
+  function searchMovies() {
+    API.search(searchTerm)
       .then((res) => {
         console.log(res.data);
         setSearchResult(res.data);
       })
-      .catch(() => {
-        setSearchString("");
+      .catch((err) => {
+        setErrorMessage(err);
+        console.log(errorMessage);
       });
   }
 
-  function loadUserMovies({ movies }) {
-    setUserMovies(movies);
+  function saveMovie() {
+    const movieData = {
+      title: searchResult.Title,
+      year: searchResult.Year,
+      rated: searchResult.Rated,
+      genre: searchResult.Genre,
+      runtime: searchResult.Runtime,
+      director: searchResult.Director,
+      actors: searchResult.Actors,
+      plot: searchResult.Plot,
+      poster: searchResult.Poster,
+      type: searchResult.Type,
+      imdbID: searchResult.imdbID,
+    };
+    API.saveMovie(movieData)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        setErrorMessage(err);
+        console.log(errorMessage);
+      });
   }
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    searchMovies();
+  };
+
+  // function loadUserMovies({ movies }) {
+  //   setUserMovies(movies);
+  // }
   return (
     <Container fluid="true">
       <Row>
         <Col size="md-12">
           <Card title="Movie Search">
             <SearchForm
-              search={searchString}
-              handleFormSubmit={handleFormSubmit}
               handleInputChange={handleInputChange}
+              handleFormSubmit={handleFormSubmit}
+              q={searchTerm}
             />
           </Card>
         </Col>
       </Row>
       <Row>
         <Col size="md-12">
-          <span>{searchResult.title ? searchResult.title : `No Results`}</span>
+          {searchResult.Title ? (
+            <Movie
+              title={searchResult.Title}
+              year={searchResult.Year}
+              rated={searchResult.Rated}
+              genre={searchResult.Genre}
+              runtime={searchResult.Runtime}
+              director={searchResult.Director}
+              actors={searchResult.Actors}
+              plot={searchResult.Plot}
+              poster={searchResult.Poster}
+              type={searchResult.Type}
+              imdbID={searchResult.imdbID}
+              saveMovie={saveMovie}
+            />
+          ) : (
+            "No Results"
+          )}
         </Col>
       </Row>
     </Container>
