622e05372a7b4088b3f2f9a2c3972012c2094307
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-14 03:59:06 -0500

    style and alignment

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -3,31 +3,33 @@ import "./SearchForm.css";
 
 function SearchForm(props) {
   return (
-    <form className="movieSearchForm">
-      <div className="form-group">
-        <label htmlFor="Search">Search:</label>
-        <input
-          className="form-control"
-          onChange={props.handleInputChange}
-          value={props.q}
-          name="search"
-          type="text"
-          placeholder="Search For a Movie"
-          id="search"
-          required
-        />
-      </div>
-      <br />
-      <div>
-        <button
-          onClick={props.handleFormSubmit}
-          type="submit"
-          className="btn btn-primary float-right"
-        >
-          Search
-        </button>
-      </div>
-    </form>
+    <div className="searchContainer">
+      <form className="movieSearchForm">
+        <div className="form-group">
+          <label htmlFor="Search">Search:</label>
+          <input
+            className="form-control"
+            onChange={props.handleInputChange}
+            value={props.q}
+            name="search"
+            type="text"
+            placeholder="Search For a Movie"
+            id="search"
+            required
+          />
+        </div>
+        <br />
+        <div>
+          <button
+            onClick={props.handleFormSubmit}
+            type="submit"
+            className="btn btn-dark float-right"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
