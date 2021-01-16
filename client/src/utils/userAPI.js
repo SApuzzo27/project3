41eef42e7e1205b711ad9c72a0cf33df1e4e5e91
Sam <SApuzzo527@gmail.com>
2021-01-16 13:31:56 -0500

    added logout function to userapi

--- client/src/utils/userAPI.js
+++ client/src/utils/userAPI.js
@@ -9,6 +9,8 @@ export default {
   },
   authenticateUser: function () {
     return axios.post("/api/user/authenticate/")
+  },
+  logoutUser: function () {
+    return axios.post("/api/user/logout")
   }
-
 };
