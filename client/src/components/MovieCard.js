a7859e5a2e93a354be16325abc271dfa8be7a4d9
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-18 18:04:07 -0500

    intial commit

---
+++ client/src/components/MovieCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function MovieCard({ movie, index }) {
+  return (
+    <div className="col-md-2" key={index}>
+      <div className="card">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://imdb.com/title/${movie.imdbID}`}
+        >
+          <img className="card-img-top" src={movie.poster} alt={movie.title} />
+        </a>
+        <div className="card-body">
+          <p className="card-text">{movie.title}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
