7e454299134c4ee6537a9868518e75c549402e36
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-19 21:48:36 -0500

    initial commit

---
+++ client/src/pages/Group.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+//import API from "../utils/API";
+import "../App.css";
+
+function Group() {
+  return (
+    <Container>
+      <Row>
+        <Col size="md-6">A Column</Col>
+        <Col size="md-6">Another Column</Col>
+      </Row>
+    </Container>
+  );
+}
