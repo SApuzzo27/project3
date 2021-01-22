d1088a8ae6639fac11a2c16e5718a0d850590610
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-22 04:06:18 -0500

    style user and group lists

--- client/src/components/GroupUsers.js
+++ client/src/components/GroupUsers.js
@@ -4,7 +4,7 @@ import Col from "./Col";
 function GroupUsers({ members }) {
   return (
     <Col size="md-6">
-      <ul className="list-group">
+      <ul className="list-group scroller">
         {members.map((item, index) => {
           return (
             <li className="list-group-item list-group-item-dark" key={index}>
