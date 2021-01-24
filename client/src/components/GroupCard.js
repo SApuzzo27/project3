a3b44916489a3554160a10944922aa8a1a50dd9f
Sam <SApuzzo527@gmail.com>
2021-01-23 19:19:03 -0500

    update to css on homepage , fully responsive, changed font color on grp main page , added borders, working on responsiveness of user crds

--- client/src/components/GroupCard.js
+++ client/src/components/GroupCard.js
@@ -1,22 +1,23 @@
 import React from "react";
 import Col from "./Col";
+import "../App.css"
 
 function GroupCard({ group, joinGroup }) {
   return (
-    <Col size="md-4">
-      <div className="card mb-3">
-        <h3 className="card-header"> {group.clubName} </h3>
+    <Col size="md-4 ">
+      <div className="card mb-3 groupCard">
+        <h3 className="card-header" style={{color:"rgb(5, 5, 93)"}}> {group.clubName} </h3>
         <div className="card-body">
           <ul className="list-group list-group-flush">
             <li className="list-group-item list-group-item-success d-flex justify-content-between align-items-right">
               {" "}
-              {group.clubName} movie Goes here!{" "}
+              {group.clubName}  Top Rated Group Movie {" "}
             </li>
             <li className="list-group-item list-group-item-warning d-flex justify-content-between align-items-right">
-              {group.clubName} movie Goes here!{" "}
+              {group.clubName}  2nd Rated Group Movie{" "}
             </li>
             <li className="list-group-item list-group-item-danger d-flex justify-content-between align-items-right">
-              {group.clubName} movie Goes here!{" "}
+              {group.clubName}  3rd Rated Group Movie{" "}
             </li>
           </ul>
           <button
