9bde963319e0110eca710976d708f3e103c84e93
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-25 01:13:46 -0500

    ES6 refactor and login error handling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,83 +1,87 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import _ from "lodash";
 import userAPI from "../utils/userAPI";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { Input, FormBtn } from "../components/Form";
 import "../components/Login.css";
 
-class Login extends Component {
-  state = {
-      email: "",
-      password: ""
-    };
-    
-  componentDidMount() {
- 
-  }
-  
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleEmailInputChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordInputChange = (e) => {
+    setPassword(e.target.value);
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    if (this.state.email && this.state.password) {
-      userAPI.loginUser({
-        email: this.state.email,
-        password: this.state.password
-      })
-        .then(res => {
-          if(res.status === 200 ){
-             this.props.setUserState(res.data)
+    if (email && password) {
+      userAPI
+        .loginUser({
+          email: email,
+          password: password,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            props.setUserState(res.data);
           }
         })
-        .catch(err => console.log(err));
+        .catch((err) => {
+          setErrorMessage(err);
+        });
     }
   };
-    refreshPage = event => {
+  const refreshPage = (event) => {
     event.preventDefault();
     window.location.reload();
-  }
+  };
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="12">
-            <form className="loginForm">
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="12">
+          <form className="loginForm">
+            {errorMessage ? (
+              <h4 className="text-danger">
+                {" "}
+                Login Error: Please try again...{" "}
+              </h4>
+            ) : (
+              <h4 className="text-dark"> Please Login or SignUp!</h4>
+            )}
+            <Input
+              value={email}
+              onChange={handleEmailInputChange}
+              name="email"
+              placeholder="email (required)"
+            />
+            <Input
+              value={password}
+              onChange={handlePasswordInputChange}
+              name="password"
+              placeholder="(required)"
+              type="password"
+            />
 
-              <Input
-                value={this.state.email}
-                onChange={this.handleInputChange}
-                name="email"
-                placeholder="email (required)"
-              />
-              <Input
-                value={this.state.password}
-                onChange={this.handleInputChange}
-                name="password"
-                placeholder="(required)"
-                type="password"
-              />
-              
-              <FormBtn 
-                disabled={!(this.state.email && this.state.password)}
-                onClick={this.handleFormSubmit}
-                >
-                Log in
-              </FormBtn>
+            <FormBtn
+              disabled={_.isEmpty(email) && _.isEmpty(password)}
+              onClick={handleFormSubmit}
+            >
+              Log in
+            </FormBtn>
             <Link to="/signup">
               <FormBtn> Signup </FormBtn>
             </Link>
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
